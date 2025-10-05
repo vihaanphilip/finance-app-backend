539941e32ef5a82a28918369c76156e8e9877b33
Vihaan Philip <vihaanphilip@gmail.com>
2025-10-05 13:04:51 +0800

    feat(frontend-routing): Change AccountTypePage routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AccountTypePage from "./pages/AccountTypePage";
 
 function App() {
   return (
-    <div>
-      <AccountTypePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<div>Welcome to Finance App</div>} />
+        <Route path="/accounttypes" element={<AccountTypePage />} />
+      </Routes>
+    </Router>
   );
 }
 
