1cd3511e8ab77f508913f9e3e70ec492bb9743a5
Vihaan Philip <vihaanphilip@gmail.com>
2026-03-19 12:24:12 +0800

    feat(FA15, 3): Ability to delete expense type

--- backend/src/main/java/com/vphilip/finance/app/expense/controller/ExpenseTypeController.java
+++ backend/src/main/java/com/vphilip/finance/app/expense/controller/ExpenseTypeController.java
@@ -4,8 +4,10 @@ import com.vphilip.finance.app.expense.model.ExpenseType;
 import com.vphilip.finance.app.expense.repository.ExpenseTypeRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/expensetypes")
@@ -24,5 +26,16 @@ public class ExpenseTypeController {
     ExpenseType createExpenseType(@RequestBody ExpenseType expenseType) {
         expenseTypeRepository.insert(expenseType);
         return expenseType;
+    }
+
+    @DeleteMapping("/{id}")
+    ExpenseType deleteExpenseType(@PathVariable Long id) {
+        Optional<ExpenseType> expenseType = expenseTypeRepository.findById(id);
+        if (expenseType.isPresent()) {
+            expenseTypeRepository.deleteById(id);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "ExpenseType not found");
+        }
+        return expenseType.get();
     }
 }
