4a0194aaa44fd83aa9810a07fcf98de2a0be283d
Vihaan Philip <vihaanphilip@gmail.com>
2026-05-24 17:58:54 +0800

    [FA-41] Import lombok dependency + simplify boilerplate for account models

--- src/main/java/com/vphilip/finance/app/account/model/Account.java
+++ src/main/java/com/vphilip/finance/app/account/model/Account.java
@@ -1,10 +1,15 @@
 package com.vphilip.finance.app.account.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "account")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Account {
 
     @Id
@@ -22,26 +27,4 @@ public class Account {
 
     @Column(name = "starting_amount")
     private BigDecimal starting_amount;
-
-    public Account() {}
-
-    public Account(Long id, String name, String description, Long account_type_id, BigDecimal starting_amount) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.account_type_id = account_type_id;
-        this.starting_amount = starting_amount;
-    }
-
-    public Long getId() { return id; }
-    public String getName() { return name; }
-    public String getDescription() { return description; }
-    public Long getAccount_type_id() { return account_type_id; }
-    public BigDecimal getStarting_amount() { return starting_amount; }
-
-    public void setId(Long id) { this.id = id; }
-    public void setName(String name) { this.name = name; }
-    public void setDescription(String description) { this.description = description; }
-    public void setAccount_type_id(Long account_type_id) { this.account_type_id = account_type_id; }
-    public void setStarting_amount(BigDecimal starting_amount) { this.starting_amount = starting_amount; }
 }
--- src/main/java/com/vphilip/finance/app/account/model/AccountType.java
+++ src/main/java/com/vphilip/finance/app/account/model/AccountType.java
@@ -1,9 +1,14 @@
 package com.vphilip.finance.app.account.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "account_type")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AccountType {
 
     @Id
@@ -15,20 +20,4 @@ public class AccountType {
 
     @Column(name = "description")
     private String description;
-
-    public AccountType() {}
-
-    public AccountType(Long id, String label, String description) {
-        this.id = id;
-        this.label = label;
-        this.description = description;
-    }
-
-    public Long getId() { return id; }
-    public String getLabel() { return label; }
-    public String getDescription() { return description; }
-
-    public void setId(Long id) { this.id = id; }
-    public void setLabel(String label) { this.label = label; }
-    public void setDescription(String description) { this.description = description; }
 }
