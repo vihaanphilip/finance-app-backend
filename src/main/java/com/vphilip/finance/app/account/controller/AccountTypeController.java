615ffa1628fc34cbe324285bec68accc8e5481bb
Vihaan Philip <vihaanphilip@gmail.com>
2026-05-25 16:17:36 +0800

    [Adhoc] change account types to v1

--- src/main/java/com/vphilip/finance/app/account/controller/AccountTypeController.java
+++ src/main/java/com/vphilip/finance/app/account/controller/AccountTypeController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/accounttypes")
+@RequestMapping("/api/v1/accounttypes")
 public class AccountTypeController {
 
     private final AccountTypeRepository accountTypeRepository;
