3f65e539249464ac587b9115c8d5c33e65a0299d
Vihaan Philip <vihaanphilip@gmail.com>
2026-05-25 14:49:37 +0800

    [FA-40] Add spring security + JWT dependencies

--- src/main/java/com/vphilip/finance/app/account/controller/AccountController.java
+++ src/main/java/com/vphilip/finance/app/account/controller/AccountController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/accounts")
+@RequestMapping("/api/v1/accounts")
 public class AccountController {
 
     private final AccountRepository accountRepository;
